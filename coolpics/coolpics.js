ffbc8e30435689a64463ab931b82fbb9aac5175e
noahpace124 <noahpace124@gmail.com>
2025-02-02 13:58:47 -0700

    Updated coolpics.

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,5 +1,41 @@
-@media screen and (min-width: 700px) {
-    .gallery {
-      grid-template-columns: 1fr 1fr;
-    }
+const menuButton = document.querySelector(".menu-button");
+function toggleMenu() {
+  const menu = document.querySelector(".menu");
+  menu.classList.toggle("hide");
+}
+menuButton.addEventListener("click", toggleMenu);
+
+function handleResize() {
+  const menu = document.querySelector(".menu");
+  if (window.innerWidth > 1000) {
+    menu.classList.remove("hide");
+  } else {
+    menu.classList.add("hide");
   }
+}
+handleResize();
+window.addEventListener("resize", handleResize);
+
+function viewerTemplate(pic, alt) {
+  return `<div class="viewer">
+    <button class="close-viewer">X</button>
+    <img src="${pic}" alt="${alt}">
+    </div>`;
+}
+
+function viewHandler(event) {
+  const clickedElement = event.target;
+
+  const src = clickedElement.getAttribute("src").split("-");
+  const newSrc = `${src[0]}-full.jpeg`;
+
+  const viewerHTML = viewerTemplate(newSrc, clickedElement.alt);
+  document.body.insertAdjacentHTML("afterbegin", viewerHTML);
+
+  document.querySelector(".close-viewer").addEventListener("click", closeViewer);
+}
+
+function closeViewer() {
+  document.querySelector(".viewer").remove();
+}
+window.addEventListener("click", viewHandler);
